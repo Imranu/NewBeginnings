aebf72ecc58405bc11719a602ead0336617c94be
charleykatan <charleykatan@gmail.com>
2022-01-19 16:19:39 +0000

    updated Goldilocks

--- src/main/java/com/qa/goldilocks/Goldilocks.java
+++ src/main/java/com/qa/goldilocks/Goldilocks.java
@@ -9,7 +9,7 @@ public class Goldilocks {
 		String output = "";
 
 		if (seatCapTemp.length <= 1) { // if seatCapTemp list is empty
-			if (Arrays.toString(seatCapTemp[0]) == "[]") { // if it outputs [] and is empty (will always have blank entry as 2d array
+			if (Arrays.toString(seatCapTemp[0]) == "[]") { // if it outputs [] and is empty (will always have blank entry as 2d array)
 				return "Seat list empty"; // output empty list message
 			}
 		}
@@ -17,7 +17,7 @@ public class Goldilocks {
 		for (int i = 0; i < seatCapTemp.length; i++) { // iterating through each index of seatCapTemp (each possible seat)
 			int [] place = seatCapTemp[i]; // returning actual value of each seat. i counting index
 			if (weight <= place[0] && maxTemp >= place[1]) { // if G can sit, add seat to output
-				output += i + 1 + " "; // i + 1 because index starts at 0 but seat number starts as 1
+				output += (i + 1) + " "; // i + 1 because index starts at 0 but seat number starts as 1
 			} 
 		}
 
