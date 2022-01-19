1b540357820ef279f58f96e1541c6288855162e8
Imran <imrau99@hotmail.co.uk>
2022-01-19 12:33:52 +0000

    updated goldilocks.java

--- src/main/java/com/qa/goldilocks/Goldilocks.java
+++ src/main/java/com/qa/goldilocks/Goldilocks.java
@@ -1,10 +1,31 @@
 package com.qa.goldilocks;
 
+import java.util.Arrays;
+
 public class Goldilocks {
 
 	public String goldilocks(int weight, int maxTemp, int[][] seatCapTemp) {
+
 		String output = "";
-		return output;
+
+		for (int i = 0; i < seatCapTemp.length; i++) {
+			int [] place = seatCapTemp[i];
+			if (seatCapTemp.length <= 1) {
+				if (Arrays.toString(seatCapTemp[0]) == "[]") {
+					return "Seat list empty";
+				}
+			}
+			if (weight < place[0] && maxTemp > place[1]) {
+				output += i + 1 + " ";
+			}
+		}
+
+		if (output.isEmpty()) {
+			return "NONE";
+		} else {
+			return output;
+		}
+
 	}
 	
 }
---
+++ src/main/java/com/qa/main/Main.java
@@ -0,0 +1,11 @@
+package com.qa.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Imran was here... Used this for testing
+
+
+    }
+
+}
