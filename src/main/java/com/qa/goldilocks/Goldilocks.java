b6065b353851691f20aa0d952a5598fdd5670955
charleykatan <charleykatan@gmail.com>
2022-01-19 11:49:34 +0000

    added Goldilocks

---
+++ src/main/java/com/qa/goldilocks/Goldilocks.java
@@ -0,0 +1,10 @@
+package com.qa.goldilocks;
+
+public class Goldilocks {
+
+	public String goldilocks(int weight, int maxTemp, int[][] seatCapTemp) {
+		String output = "";
+		return output;
+	}
+	
+}
